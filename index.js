1d4b14bb51971ef4371561f3f5829838243c3a36
Aminah Rashid <aminahrashid.bsaif20@iba-suk.edu.pk>
2024-07-01 16:36:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,46 +7,35 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const IPINFO_API_KEY = process.env.IPINFO_API_KEY;
-const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
-app.get("/api/hello", async (req, res) => {
+app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || "Guest";
-  
-    // Get the client's IP address from 'x-forwarded-for' header or fallback to req.ip
-    const clientIp = req.headers["x-forwarded-for"]
-      ? req.headers["x-forwarded-for"].split(",")[0]
-      : req.connection.remoteAddress;
-    console.log(req.connection.remoteAddress);
-    
+
+    const clientIp = req.headers['x-forwarded-for']
+        ? req.headers['x-forwarded-for'].split(',')[0]
+        : req.connection.remoteAddress;
+
     try {
-      // Fetch location data from ipinfo
-      const locationResponse = await axios.get(
-        `https://ipinfo.io/${clientIp}?token=${IPINFO_API_KEY}`
-      );
-      const locationData = locationResponse.data;
-      const city = locationData.city;
-      const country = locationData.country;
-  
-      // Fetch weather data from OpenWeatherMap
-      const weatherResponse = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHERMAP_API_KEY}&units=metric`
-      );
-      const weatherData = weatherResponse.data;
-      const temperature = weatherData.main.temp;
-  
-      // Respond with the gathered data
-      res.json({
-        client_ip: clientIp,
-        location: city,
-        greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${city}`,
-      });
+        // Fetch location and weather data from WeatherAPI
+        const weatherResponse = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${clientIp}`);
+        const weatherData = weatherResponse.data;
+        
+        const city = weatherData.location.name;
+        const temperature = weatherData.current.temp_c;
+
+        // Respond with the gathered data
+        res.json({
+            client_ip: clientIp,
+            location: city,
+            greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${city}`
+        });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Unable to get location or weather data" });
+        console.error(error);
+        res.status(500).json({ error: "Unable to get location or weather data" });
     }
-  });
-  
-  app.listen(port, () => {
+});
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+});
